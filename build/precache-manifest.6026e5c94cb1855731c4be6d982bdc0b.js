af1c7e0da2fb954f4d6cd71cd3b2be2d0a511123
gisellehargrove <gisellehargrove@gmail.com>
2019-08-02 09:12:19 -0400

    build production

---
+++ build/precache-manifest.6026e5c94cb1855731c4be6d982bdc0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e234ded027f3d7df1aad8f9c11ecba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "935781f7188699ce95f2",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e84d3e91caeb544a0e2",
+    "url": "/static/js/2.3dbe609e.chunk.js"
+  },
+  {
+    "revision": "935781f7188699ce95f2",
+    "url": "/static/js/main.31b4566d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
