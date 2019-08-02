bd79084a84a4034d834ac2e5731ad0f8f68d3a8a
gisellehargrove <gisellehargrove@gmail.com>
2019-08-02 10:38:57 -0400

    update build(2)

---
+++ build/precache-manifest.030316991243a1418758620ade982779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ce6ee463b6a2dd430200a5a352506c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d6924ebef5b0b43a428",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e36e47114b98b47b862a",
+    "url": "/static/js/2.d0046a61.chunk.js"
+  },
+  {
+    "revision": "4d6924ebef5b0b43a428",
+    "url": "/static/js/main.29a072b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
